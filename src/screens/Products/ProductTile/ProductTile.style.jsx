43944a8440211b8d11c57d41f3c6fe8491a7848d
Michael-Zou-MZ <112382791+Michael-Zou-MZ@users.noreply.github.com>
2023-05-09 20:24:03 +1000

    Update ProductTile.style.jsx

--- src/screens/Products/ProductTile/ProductTile.style.jsx
+++ src/screens/Products/ProductTile/ProductTile.style.jsx
@@ -3,23 +3,19 @@ import styled from "styled-components";
 
 export const ProductTile = styled.div`
   color: #444;
-  border-radius: 20px;
   text-align: center;
   margin-bottom: 30px;
-  height: 180px;
+  width:100%;
   padding:20px 20px 30px 20px;
   background-color: white;
-  opacity: 0.95;
   transition: all ease 0.4s;
-  align-verital: middle;
   
   &:hover {
-    transform: scale(0.95);
+    scale:0.95;
   }
 `
 export const ProductIcon = styled.div`
   > svg {
-    margin-top: 30px;
     font-size:40px;
     color:#1ab4fc;
   }    
@@ -31,14 +27,11 @@ export const ProductTitle = styled.div`
   font-family: "Poppins ExtraBold",sans-serif;
   font-size:20px;
   color:#0a1e38;
-  
 `
 
 export const ProductSubtitle = styled.div`
   font-family: "Open Sans",sans-serif;
   display: block;
-  margin-right: 15px;
-  margin-left: 15px;
 `
 
 export const ProductLink = styled.a`
