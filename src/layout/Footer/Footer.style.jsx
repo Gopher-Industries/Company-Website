f45e64b736e539bb4241ac65eea4b2bc502e4066
Michael-Zou-MZ <112382791+Michael-Zou-MZ@users.noreply.github.com>
2023-05-09 20:16:33 +1000

    Update Footer.style.jsx

--- src/layout/Footer/Footer.style.jsx
+++ src/layout/Footer/Footer.style.jsx
@@ -1,41 +1,19 @@
 import styled from "styled-components";
 
 export const FooterContainer = styled.div`
-  background: rgb(10,30,56);
-  width: 100%;
+  background: #0a1e38;
   color: #fff;
   font-size: 14px;
-
   width:100%;
   box-sizing: border-box;
-
-  //tablet
-  @media (min-width: 768px) and (max-width: 991px) {
-    font-size: 12px;
-  }
-
-  //mobile
-  @media only screen and (max-width: 767px) {
-    font-size: 12px;
-  }
 `
 
 export const FooterTop = styled.div`
-  background: rgb(10,30,56);
+  background: #0a1e38;
   padding: 60px 0 30px 0;
   box-sizing:border-box;
   display:flex;
   justify-content: center;
-
-  //tablet
-  @media (min-width: 768px) and (max-width: 991px) {
-    padding: 40px 30px 20px 30px;
-  }
-
-  //mobile
-  @media only screen and (max-width: 767px) {
-    padding: 40px 30px 20px 30px;
-  }
 `
 
 export const FooterTopContent = styled.div`
@@ -43,21 +21,6 @@ export const FooterTopContent = styled.div`
   justify-content: space-between;
   display:grid;
   grid-template-columns: repeat(2, 1fr);
-
-  //tablet
-  @media (min-width: 768px) and (max-width: 991px) {
-    display:grid;
-    grid-template-columns: repeat(2, 1fr);
-  }
-
-  //mobile
-  @media only screen and (max-width: 767px) {
-    display:grid;
-    grid-template-columns: repeat(1, 1fr);
-    align-items: center;
-    justify-items: center; 
-    grid-row-gap: 25px;
-  }
 `
 
 export const FooterTitle = styled.div`
@@ -70,15 +33,14 @@ export const FooterTitle = styled.div`
 `
 
 export const FooterSocials = styled.div`
-
 `
 
 export const FooterBottom = styled.div`
   text-align: center;
-  padding:30px 0 0 0;
+  padding:30px 0 30px 0;
   font-family: "Open Sans",sans-serif;
 `
-{/*
+
 export const FooterTopLeft = styled.div`
 `
 
@@ -86,50 +48,8 @@ export const FooterTopRight = styled.div`
   width:600px;
   text-align: center;
   font-family: "Open Sans",sans-serif;
-
-  //tablet
-  @media (min-width: 768px) and (max-width: 991px) {
-    padding:20px 0 30px 0;
-  }
-
-  //mobile
-  @media only screen and (max-width: 767px) {
-    padding:20px 0 30px 0;
-  }
-`
-*/}
-
-export const FooterText = styled.div`
-  text-align: center;
-  margin-top: 30px;
-
-`
-
-export const FooterTopLeft = styled.div`
-
-`
-
-export const FooterTopRight = styled.div`
-  width:600px;
-  text-align: center;
-  font-family: "Open Sans",sans-serif;
-
-  //tablet
-  @media (min-width: 768px) and (max-width: 991px) {
-      width: 460px;
-  }
-  
-  //mobile
-  @media only screen and (max-width: 767px) {
-      width: 390px;
-  }
-
 `
 export const Logo = styled.img`
   height:55px;
-  width: 200px;
-  margin-left: 5px;
-  align-self: center;
   padding-bottom:20px;
 `
-
