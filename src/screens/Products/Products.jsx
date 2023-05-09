0a37ed02e38c81e230e14049ea669ecc1caf0570
Michael-Zou-MZ <112382791+Michael-Zou-MZ@users.noreply.github.com>
2023-05-09 20:24:51 +1000

    Update Products.jsx

--- src/screens/Products/Products.jsx
+++ src/screens/Products/Products.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import HomeVideo from "@Assets/videos/connections.mp4";
 import * as s from "./Products.style";
 import ProductTile from "./ProductTile/ProductTile";
-import {Container, Row, Col} from 'react-grid-system';
+
 import MedicationLiquidIcon from '@mui/icons-material/MedicationLiquid';
 import HealthAndSafetyIcon from '@mui/icons-material/HealthAndSafety';
 import CollectionsBookmarkIcon from '@mui/icons-material/CollectionsBookmark';
@@ -13,56 +13,25 @@ import SpaIcon from '@mui/icons-material/Spa';
 const Products = () => {
     return (
         <s.ProductContainer>
-            <s.ProductVideo src={HomeVideo} width="100%" height='100vh%' controls={false} autoPlay={true} loop={true} muted={true}/>
-            
-            <Container style={{height: '100%'}}>
-                <div style={{height: '200px'}}/>
-                <Row>
-                    <Col lg={4} md={6}>
-                        <s.ProductLink href="/products/medimind"><ProductTile icon={<MedicationLiquidIcon />} title={"MediMind"} subtitle={"Medication Management and Reminder App"} /></s.ProductLink>
-                    </Col>
-                    <Col lg={4} md={6}>
-                        <s.ProductLink href="/products/apollo"><ProductTile icon={<CollectionsBookmarkIcon />} title={"Apollo Database"} subtitle={"Standardised Patient Database"} /></s.ProductLink>
-                    </Col>
-                    <Col lg={4} md={6}>
-                        <s.ProductLink href="/products/guardian"><ProductTile icon={<HealthAndSafetyIcon />} title={"Guardian App"} subtitle={"Elderly Assistance App"} /></s.ProductLink>
-                    </Col>
-                    <Col lg={4} md={6}>
-                        <s.ProductLink href="/products/chatbot"><ProductTile icon={<HeadsetMicIcon />} title={"Medi"} subtitle={"Chatbot System for Remote Health Monitoring"} /></s.ProductLink>
-                    </Col>
-                    <Col lg={4} md={6}>
-                        <s.ProductLink href="/products/painassessment"><ProductTile icon={<SickIcon />} title={"Pain Assessment"} subtitle={"AI powered pain assessor"} /></s.ProductLink>
-                    </Col>
-                    <Col lg={4} md={6}>
-                        <s.ProductLink href="/products/dietplanner"><ProductTile icon={<SpaIcon />} title={"NutriHelp"} subtitle={"Personalised diet planning Android app"} /></s.ProductLink>
-                    </Col>
-                </Row>
-                <div style={{height: '200px'}}/>
-            </Container>
-
-
-
-
-            {/* 
+            <s.ProductVideo src={HomeVideo} width="100%" controls={false} autoPlay={true} loop={true} muted={true}/>
             <s.ProductInnerContainer>
                 <s.ProductRow>
                     <s.ProductHeader>Products</s.ProductHeader>
                     <s.ProductText>Our company is producing a range of products that meet the goals and objectives of the company.</s.ProductText>
                     <s.ProductItemsContainer>
                         <s.ProductItemsRow>
-
                             <ProductTile icon={<MedicationLiquidIcon />} title={"MediMind"} subtitle={"Medication Management and Reminder App"} link={"products/medimind"} linkTitle={"View More"} />
                             <ProductTile icon={<CollectionsBookmarkIcon />} title={"Apollo Database"} subtitle={"Standardised Patient History Database"} link={"products/apollo"} linkTitle={"View More"}  />
                             <ProductTile icon={<HealthAndSafetyIcon />} title={"Guardian App"} subtitle={"Elderly Assistance App"} link={"products/guardian"} linkTitle={"View More"} />
+                        </s.ProductItemsRow>
+                        <s.ProductItemsRow>
                             <ProductTile icon={<HeadsetMicIcon />} title={"Medi"} subtitle={"Chatbot System for Remote Health Monitoring"} link={"products/chatbot"} linkTitle={"View More"} />
                             <ProductTile icon={<SickIcon />} title={"Pain Assessment"} subtitle={"AI powered pain assessor"} link={"products/painassessment"} linkTitle={"View More"} />
                             <ProductTile icon={<SpaIcon />} title={"NutriHelp"} subtitle={"Personalised diet planning Android app"} link={"products/dietplanner"} linkTitle={"View More"} />
-
                         </s.ProductItemsRow>
                     </s.ProductItemsContainer>
                 </s.ProductRow>
             </s.ProductInnerContainer>
-            */}
         </s.ProductContainer>
     );
 };
