8b61543f97369fc6368fcc383879137a6e8592a7
Michael-Zou-MZ <112382791+Michael-Zou-MZ@users.noreply.github.com>
2023-05-10 18:07:20 +1000

    Update ProductImage.style.jsx

--- src/components/ProductImage/ProductImage.style.jsx
+++ src/components/ProductImage/ProductImage.style.jsx
@@ -10,24 +10,12 @@ export const ProductImageContainer = styled.div`
   background-repeat: no-repeat;
   background-position: center;
   display: block;
-  width:340px;
-  height:340px;
-  margin-top: 20px;
-  margin-right: 20px;
-  margin-bottom: 30px;
+  width:200px;
+  height:200px;
   transition: all ease 0.4s;
-  justify-content: space-around;
   
   &:hover {
     scale:0.95;
     cursor:pointer;
-  }
-
-  @media only screen and (max-width: 1600px) {
-    margin-right: 20px;
-    width:250px;
-    height:250px;
-    margin-left: 15px;
-    margin-right: 15px;
   }
 `
