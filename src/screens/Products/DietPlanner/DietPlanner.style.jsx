d86f47dfa66762f31aec7ffdf5718f3b79de5910
Michael-Zou-MZ <112382791+Michael-Zou-MZ@users.noreply.github.com>
2023-05-10 19:07:34 +1000

    Update DietPlanner.style.jsx

--- src/screens/Products/DietPlanner/DietPlanner.style.jsx
+++ src/screens/Products/DietPlanner/DietPlanner.style.jsx
@@ -8,7 +8,7 @@ export const ProductContainer = styled.div`
 
 export const ProductInnerContainer = styled.div`
   width:100%;
-  height: 320px;
+
 `
 
 export const ProductRow = styled.div`
@@ -26,7 +26,7 @@ export const ProductPageHeader = styled.h2`
   font-weight: 700;
   font-family: "Poppins ExtraBold", sans-serif;
   margin:0;
-  font-size:60px;
+  font-size:40px;
   line-height:60px;
   letter-spacing: -2px;
   text-align: center;
@@ -37,7 +37,6 @@ export const ProductPageText = styled.div`
   margin-top: 20px;
   margin-right: 15px;
   margin-left: 15px;
-  font-size:20px;
   font-family: "Open Sans",sans-serif;
   text-align: center;
 `
@@ -52,7 +51,7 @@ export const ProductTop = styled.div`
 
 export const PageVideo = styled.video`
   width: 100%;
-  height: 320px;
+  height:320px;
   object-fit: cover;
   position: fixed;
   left: 0;
@@ -60,6 +59,15 @@ export const PageVideo = styled.video`
   top: 0;
   bottom: 0;
   z-index: -1;
+`
+
+export const ProductDescGradient = styled.div`
+  background: linear-gradient(225deg,rgba(63,184,175,1) 0%,rgba(20,157,215,1) 100%);
+  padding: 30px 0 30px 0;
+  justify-content: space-around;
+  display: flex;
+  flex-direction: row;
+  margin:0 auto;
 `
 
 export const ProductContentRow = styled.div`
@@ -71,54 +79,27 @@ export const ProductContentRow = styled.div`
   background-color: white;
 `
 
-export const ProductDescGradient = styled.div`
-  background: linear-gradient(225deg,rgba(63,184,175,1),rgba(20,157,215,1));
-  width: 100%;
-  justify-content: center;
-`
-
-export const ProductDescGradient2 = styled.div`
-  background: linear-gradient(225deg,rgba(63,184,175,1),rgba(20,157,215,1));
-  width: 100%;
-  justify-content: center;
-  height: 300px;
-  @media (max-width: 767px) {
-    height: 400px;
-  }
-  @media (max-width: 426px) {
-    height: 450px;
-  }
-`
-
 export const ProductTitle = styled.h1`
+  font-weight: 700;
+  font-size:70px;
   font-family: "Poppins ExtraBold", sans-serif;
   line-height:60px;
   letter-spacing: -3px;
   text-align: left;
   color: rgb(10, 30, 56);
-  margin-top: 70px;
+  margin-top: 50px;
   text-transform: uppercase;
-  @ media only screen and (max-width: 576px) {
-    margin-left: 15px;
-    margin-right: 15px;
-  }
+`
+
+export const ProductTitleWhite = styled(ProductTitle)`
+  color:white;
+  padding-bottom: 50px;
 `
 
 export const ProductText = styled.div`
   color:black;
   font-family: "Open Sans",sans-serif;
-  margin-bottom: 50px;
-  @media only screen and (max-width: 1600px) {
-    margin-bottom: 50px;
-    margin-left: 15px;
-    margin-right: 15px;
-  }
-  @media only screen and (max-width: 1200px) {
-    margin-bottom: -50px;
-    margin-left: 15px;
-    margin-right: 15px;
-  }
-
+  margin-bottom: 100px;
 `
 
 export const ProductVideo = styled.video`
@@ -127,76 +108,69 @@ export const ProductVideo = styled.video`
   border-radius: 5px;
   box-shadow: 5px 5px 24px -1px #5e5e5e;
   display: flex;
-  margin-top: 60px;
-  margin-bottom: 50px;
-  @media only screen and (max-width: 1600px) {
-    margin-top: 110px;
-  }
+  margin-top: 150px;
+  margin-bottom: 150px;
 `
 
-export const FeatureText = styled.li`
+export const FeatureText = styled.ul`
   margin-top: 20px;
   margin-right: 30px;
-  font-weight:bold;
-  list-style-type: none;
+`
 
-  @media only screen and (max-width: 992px) {
-    list-style-type: disc;
-    list-style-position: outside;
-    margin-left: 15px;
-    margin-right: 15px;
-  }
+export const BenefitsText = styled.ul`
+  margin-top: 20px;
+  margin-right: 30px;
+  color: white;
+  padding-bottom: 
 `
 
 {/* 
-
-export const ProductSubColumns = styled.div`
-  display:flex;
-  flex-direction: row;
-  color:black;
-  gap:20px;
+export const ProductContentRowBlue = styled(ProductContentRow)`
+  background-color: rgb(10,30,56);
 `
 
 export const ProductContentRowInner = styled.div`
-  width: 1200px;
+  max-width:1200px;
   display: flex;
   flex-direction: row;
-  @media only screen and (max-width: 767px){
-    flex-direction: column;
-  }
 `
 
 export const ProductContentLeft = styled.div`
-  width:50%;
-  margin-left: 20px;
-  margin-right: 20px;
-  @media only screen and (max-width: 767px){
-    width: 100%;
-    margin-left: 5%;
-    margin-right: 0%;
-  }
+  max-width:55%;
 `
 
 export const ProductContentRight = styled.div`
-  width:50%;
+  max-width:45%;
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
   box-sizing: border-box;
   padding-left:20px;
-  @media only screen and (max-width: 767px){
-    width: 100%;
-    margin-left: 5%;
-    margin-right: 5%;
-    padding-left:0px;
-  }
 `
 
 export const ProductContent = styled.div`
 `
+*/}
 
-export const FeatureList = styled.div`
+{/*
+export const ProductSubColumns = styled.div`
+  display:flex;
+  flex-direction: row;
+  color:black;
+  gap:20px;
+  
+  > div {
+    max-width:33%;
+  }
+`
+
+export const ProductSubColumnsWhite = styled(ProductSubColumns)`
+  color:white;
+  justify-content: space-between;
+`
+
+export const FeatureList = styled.ul`
   margin:0;
   padding-inline-start: 20px;
   list-style:none;
@@ -214,28 +188,24 @@ export const FeatureList = styled.div`
     margin-left: -1em;
   }
 `
-*/}
 
-export const ProductImage = styled.img`
-  width: 340px;
-  height: 340px;
-  margin-right: 20px;
-  margin-bottom: 30px;
-  object-fit: cover;
-  background-position: cover;
-  border-radius: 15px;
-  display: inline-block;
-  justify-content: space-around;
-  transition: all ease 0.4s;
-  &:hover,
-  &:focus {
-    cursor: pointer;
-
+export const FeatureListWhite = styled.ul`
+  margin:0;
+  padding-inline-start: 20px;
+  list-style:none;
+  
+  > li {
+    padding-bottom:15px;
+    color:white;
   }
-
-  @media only screen and (max-width: 1540) {
-    width: 500px;
-    height: 500px;
-    margin-bottom: 50px;
+  
+  > li::before {
+    content: "\\2022";
+    color: white;
+    font-weight: bold;
+    display: inline-block;
+    width: 1em;
+    margin-left: -1em;
   }
 `
+*/}
